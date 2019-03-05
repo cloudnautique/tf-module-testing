8d4f11128823000e5dd2921395321cfa0f9d975b
Bill Maxwell <bill@rancher.com>
2019-03-05 10:04:07 -0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "rancher2" {
+  api_url    = "${var.api_url}"
+  access_key = "${var.rancher2_access_key}"
+  secret_key = "${var.rancher2_secret_key}"
+  insecure   = "${var.rancher2_tls_insecure}"
+}
+
+resource "rancher2_cluster" "test_cluster" {
+  name        = "test-cluster-000"
+  description = "testing"
+  kind        = "rke"
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "api_url" {}
+
+variable "rancher2_access_key" {}
+
+variable "rancher2_secret_key" {}
+
+variable "rancher2_tls_insecure" {}
